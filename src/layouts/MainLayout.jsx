b512046083e80740238deb96d99cc063a578399d
vtlxng <vietlong.eee@gmail.com>
2025-08-13 09:15:34 +0700

    change layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,10 @@
 import Navbar from '../components/Navbar';
 import { generatePlaceholderAvatar } from '../utils/placeholderImages';
+import { Link, useLocation } from 'react-router-dom';
 
 const MainLayout = ({ children }) => {
+    const location = useLocation();
+
     return (
         <div className="min-vh-100" style={{ backgroundColor: 'var(--fb-background)' }}>
             {/* Navigation */}
@@ -14,26 +17,33 @@ const MainLayout = ({ children }) => {
                     <div className="col-xl-3 col-lg-3 d-none d-lg-block">
                         <div className="sidebar-fb position-sticky" style={{ top: '70px' }}>
                             <div className="p-3">
-                                <a href="#" className="sidebar-item-fb active">
+                                <Link
+                                    to="/"
+                                    className={`sidebar-item-fb ${location.pathname === '/' ? 'active' : ''}`}
+                                >
                                     <svg className="me-3" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                                         <path d="M9.464 1.286C10.294.803 11.092.5 12 .5c.908 0 1.706.303 2.536.786.795.462 1.697 1.14 2.815 1.977l2.232 1.675c1.391 1.042 2.359 1.766 2.888 2.826.53 1.059.529 2.268.529 4.178V17.25c0 .966-.784 1.75-1.75 1.75H4.75A1.75 1.75 0 0 1 3 17.25v-5.312c0-1.91-.001-3.119.529-4.178.53-1.06 1.497-1.784 2.888-2.826L8.649 3.249c1.118-.837 2.02-1.515 2.815-1.977z" />
                                     </svg>
                                     Bảng tin
-                                </a>
-                                <a href="#" className="sidebar-item-fb">
+                                </Link>
+                                <Link
+                                    to="/friends"
+                                    className={`sidebar-item-fb ${location.pathname === '/friends' ? 'active' : ''}`}
+                                >
                                     <svg className="me-3" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                                         <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 3a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm0 14.2a7.2 7.2 0 0 1-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 0 1-6 3.22z" />
                                     </svg>
                                     Bạn bè
-                                </a>
-                                <a href="#" className="sidebar-item-fb">
-                                    <svg className="me-3" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                                        <path d="M12 4a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 4a3 3 0 1 0 0 6 3 3 0 0 0 0-6z" />
-                                        <path fillRule="evenodd" d="M13 14H3a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1z" />
-                                        <path fillRule="evenodd" d="M14 8a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h12z" />
+                                </Link>
+                                <Link
+                                    to="/messages"
+                                    className={`sidebar-item-fb ${location.pathname === '/messages' ? 'active' : ''}`}
+                                >
+                                    <svg className="me-3" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
+                                        <path d="M12 2C6.477 2 2 6.477 2 12c0 1.89.525 3.66 1.438 5.17L2.546 20.4a1 1 0 0 0 1.053 1.054l3.23-.892A9.958 9.958 0 0 0 12 22c5.523 0 10-4.477 10-10S17.523 2 12 2z" />
                                     </svg>
-                                    Lời mời kết bạn
-                                </a>
+                                    Tin nhắn
+                                </Link>
                             </div>
                         </div>
                     </div>
